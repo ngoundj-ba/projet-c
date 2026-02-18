ba4f584795f0133f6fc3ad907254f5d7354f5552
ngoundj-ba <ngoundjb@gmail.com>
2026-02-18 12:31:32 +0000

    n entier negatif

--- Untitled2.c
+++ Untitled2.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+int n,x,i,somme=0,cpt=0;
+float moy;
+do{
+    printf("saisir n entier");
+    scanf("%d",&n);
+}while(n<0);
+for(i=0;i<n;i++){
+    printf("saisir une valeur negatif");
+    scanf("%d",&x);
+}while(x<0);
+printf("le nombre que vous avez saisie est:%d",x);
+}
